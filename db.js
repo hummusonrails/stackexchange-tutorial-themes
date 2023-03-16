315a1c76e64dcc2e2e3250224e65ea7e59de2496
Ben Greenberg <bengreenberg@gmail.com>
2023-03-16 19:17:49 +0200

    update db connection string

--- db.js
+++ db.js
@@ -4,8 +4,11 @@ import { config } from 'dotenv';
 
 config();
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-  host: process.env.DATABASE_URL || process.env.DB_HOST,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
